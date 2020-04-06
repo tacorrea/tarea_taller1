91fbe30193bf80361ed06622acb5d7eef991af14
Pablo Correa <pablocorrea@MacBook-Pro-de-Pablo.local>
2020-04-05 20:06:32 -0400

    secret key

--- prueba/settings.py
+++ prueba/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('BLOG_KEY')
+SECRET_KEY = 'w$-#krbw&oi&d!70nk8#ncsje*%ixfy3_jklc&akdy%hgw@ihd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
